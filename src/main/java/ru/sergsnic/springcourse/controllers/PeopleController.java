8de7cac230b7ed3edc8dd569b4158ca44fcbc258
Sergey <sergsnic@mail.ru>
2023-10-05 15:58:30 +0300

    add Task 23 CRUD - PATCH DELETE add filter in file config/MySpringMvcDispatcherSerlvetIntitializer

--- src/main/java/ru/sergsnic/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sergsnic/springcourse/controllers/PeopleController.java
@@ -29,13 +29,33 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person){
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
+
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") Person person) {
         personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
         return "redirect:/people";
     }
 }
