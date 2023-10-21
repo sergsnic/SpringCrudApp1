8b5757198a1dd9a053f0df9d45e46cfe7c1fdef8
Sergey <sergsnic@mail.ru>
2023-10-21 12:44:21 +0300

    add Task41 Spring Validator

--- src/main/java/ru/sergsnic/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sergsnic/springcourse/controllers/PeopleController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.sergsnic.springcourse.dao.PersonDAO;
 import ru.sergsnic.springcourse.models.Person;
+import ru.sergsnic.springcourse.util.PersonValidator;
 
 import javax.validation.Valid;
 
@@ -15,12 +16,13 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
-
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
@@ -41,6 +43,7 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -57,6 +60,7 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/edit";
 
--- src/main/java/ru/sergsnic/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/sergsnic/springcourse/dao/PersonDAO.java
@@ -6,9 +6,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.sergsnic.springcourse.models.Person;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+
 
 @Component
 public class PersonDAO {
@@ -22,6 +23,10 @@ public class PersonDAO {
 
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[]{email},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
     public Person show(int id) {
